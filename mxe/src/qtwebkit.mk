383a72cb05a59e73f3e48af9d3e7679932004dd3
margin <support@margin.de>
2019-02-11 17:38:36 +0100

    removed tests and added debug build step to qtwebkit

---
+++ mxe/src/qtwebkit.mk
@@ -0,0 +1,62 @@
+# This file is part of MXE. See LICENSE.md for licensing information.
+
+PKG             := qtwebkit
+$(PKG)_WEBSITE  := https://github.com/annulen/webkit
+$(PKG)_DESCR    := QtWebKit
+$(PKG)_IGNORE   :=
+$(PKG)_VERSION  := 5.212
+$(PKG)_CHECKSUM := 283b907ea324a2c734e3983c73fc27dbd8b33e2383c583de41842ee84d648a3e
+$(PKG)_SUBDIR   := qtwebkit-everywhere-src-$($(PKG)_VERSION)
+$(PKG)_FILE     := qtwebkit-everywhere-src-$($(PKG)_VERSION).tar.xz
+$(PKG)_URL      := https://download.qt.io/snapshots/ci/qtwebkit/$($(PKG)_VERSION)/latest/src/submodules/$($(PKG)_FILE)
+$(PKG)_DEPS     := cc libxml2 libxslt libwebp qtbase qtmultimedia qtquickcontrols \
+                   qtsensors qtwebchannel sqlite
+
+define $(PKG)_BUILD_SHARED
+    cd '$(BUILD_DIR)/release' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DCMAKE_INSTALL_PREFIX=$(PREFIX)/$(TARGET)/qt5 \
+        -DCMAKE_CXX_FLAGS='-fpermissive' \
+	-DCMAKE_BUILD_TYPE='Release' \
+        -DEGPF_DEPS='Qt5Core Qt5Gui Qt5Multimedia Qt5Widgets Qt5WebKit' \
+        -DPORT=Qt \
+        -DENABLE_GEOLOCATION=OFF \
+        -DENABLE_MEDIA_SOURCE=ON \
+        -DENABLE_VIDEO=ON \
+        -DENABLE_WEB_AUDIO=ON \
+        -DUSE_GSTREAMER=OFF \
+        -DUSE_MEDIA_FOUNDATION=OFF \
+        -DUSE_QT_MULTIMEDIA=ON
+    $(MAKE) -C '$(BUILD_DIR)/release' -j '$(JOBS)' VERBOSE=1 || $(MAKE) -C '$(BUILD_DIR)/release' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)/release' -j 1 install
+
+
+    cd '$(BUILD_DIR)/debug' && $(TARGET)-cmake '$(SOURCE_DIR)' \
+        -DCMAKE_INSTALL_PREFIX=$(PREFIX)/$(TARGET)/qt5 \
+        -DCMAKE_CXX_FLAGS='-fpermissive' \
+	-DCMAKE_BUILD_TYPE='Debug' \
+        -DEGPF_DEPS='Qt5Core Qt5Gui Qt5Multimedia Qt5Widgets Qt5WebKit' \
+        -DPORT=Qt \
+        -DENABLE_GEOLOCATION=OFF \
+        -DENABLE_MEDIA_SOURCE=ON \
+        -DENABLE_VIDEO=ON \
+        -DENABLE_WEB_AUDIO=ON \
+        -DUSE_GSTREAMER=OFF \
+        -DUSE_MEDIA_FOUNDATION=OFF \
+        -DUSE_QT_MULTIMEDIA=ON
+    $(MAKE) -C '$(BUILD_DIR)/debug' -j '$(JOBS)' VERBOSE=1 || $(MAKE) -C '$(BUILD_DIR)/debug' -j '$(JOBS)' VERBOSE=1
+    $(MAKE) -C '$(BUILD_DIR)/debug' -j 1 install
+
+    # build test manually
+    # add $(BUILD_TYPE_SUFFIX) for debug builds - see qtbase.mk
+#   $(TARGET)-g++ \
+#        -W -Wall -std=c++11 \
+#        '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-$(PKG).exe' \
+#        `$(TARGET)-pkg-config Qt5WebKitWidgets --cflags --libs`
+
+    # batch file to run test programs
+#    (printf 'set PATH=..\\lib;..\\qt5\\bin;..\\qt5\\lib;%%PATH%%\r\n'; \
+#     printf 'set QT_QPA_PLATFORM_PLUGIN_PATH=..\\qt5\\plugins\r\n'; \
+#     printf 'test-$(PKG).exe\r\n'; \
+#     printf 'cmd\r\n';) \
+#     > '$(PREFIX)/$(TARGET)/bin/test-$(PKG).bat'
+endef
